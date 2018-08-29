695f725ba96effcf4b1f87c63403e40e319a543b
Samer <samerstudio@outlook.com>
2018-08-29 13:02:32 -0300

    Setup departe test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,7 @@
 var mongoose = require('mongoose')
-var ip_local = 'mongodb://localhost:27017'
-var ip_online = 'mongodb://eladmin:hola123@ds021434.mlab.com:21434/todo'
-
-if(process.env.PORT){
-  var ip = ip_online;
-}else{
-  var ip = ip_local;
-}
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://eladmin:hola123@ds021434.mlab.com:21434/todo', {useNewUrlParser: true}).then(() => {
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then(() => {
   console.log('Connected to db')
 }).catch((e) => {
   console.log('Failed to connect db', e)

6c52ddb4b5111db9ad11a06fa60dc4ce37844a3a
Samer <samerstudio@outlook.com>
2018-08-28 22:36:26 -0300

    Fixed problem connectin to the server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://eladmin:hola123>@ds021434.mlab.com:21434/todo', {useNewUrlParser: true})
+mongoose.connect('mongodb://eladmin:hola123@ds021434.mlab.com:21434/todo', {useNewUrlParser: true}).then(() => {
+  console.log('Connected to db')
+}).catch((e) => {
+  console.log('Failed to connect db', e)
+})
 
 module.exports = {mongoose}

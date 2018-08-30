b2b7f19eb8a483c32c26378caca0a07814081a1c
Samer <samerstudio@outlook.com>
2018-08-30 16:02:04 -0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,27 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-}
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc')
-console.log('decoded ',decoded)
+var password = '123abc';
+
+bcrypt.genSalt(10, (err,salt) => {
+  bcrypt.hash(password,salt,(err,hash) => {
+    console.log(hash)
+  })
+})
+
+var hashedPassword = '$2a$10$8Hx.io3GebmLa1GOvaTdze34ohdwurz0/NFwmhQigjwDZNmSuZPsW'
+
+bcrypt.compare(password, hashedPassword, (err,result) => {
+  console.log(result)
+})
+// var data = {
+//   id: 10
+// }
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc')
+// console.log('decoded ',decoded)
 // jwr.verify
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();

b758f5e33b20a566c34c5f785c3f7cc6b7244378
Samer <samerstudio@outlook.com>
2018-09-05 14:29:29 -0300

    Solved config env variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
-if(env === 'development'){
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-}else if(env === 'test') {
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-}else{
-  process.env.MONGODB_URI = 'mongodb://eladmin:hola123@ds021434.mlab.com:21434/todo'
+
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json')
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  })
 }
--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString()}, 'estaeslaposta').toString()
+  var token = jwt.sign({_id: user._id.toHexString()}, process.env.JWT_SECRET).toString()
 
   user.tokens = user.tokens.concat([{access,token}]);
   user.save().then(() => {
@@ -57,7 +57,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try{
-    decoded = jwt.verify(token, 'estaeslaposta')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   }catch (e) {
     return Promise.reject();
   }
